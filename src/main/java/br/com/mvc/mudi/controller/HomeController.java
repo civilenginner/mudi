6c26cb7b91bc948fb39289a30ac906f86617b7b0
wagner maidson <wagner.rmaidson@gmail.com>
2020-11-07 03:12:09 -0300

    create redirect by status order

--- src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -3,25 +3,43 @@ package br.com.mvc.mudi.controller;
 import java.util.List;
 
 
+import br.com.mvc.mudi.model.StatusPedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.mvc.mudi.controller.repository.PedidoRepository;
+import br.com.mvc.mudi.repository.PedidoRepository;
 import br.com.mvc.mudi.model.Pedido;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     @Autowired
     private PedidoRepository pedidoRepository;
 
-    @GetMapping("/home")
+    @GetMapping
     public String home(Model model) {
         List<Pedido> pedidos = pedidoRepository.findAll();
         model.addAttribute("pedidos", pedidos);
         return "home";
+    }
+
+    @GetMapping("/{status}")
+    public String porStatus(@PathVariable("status") String status, Model model) {
+        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+        model.addAttribute("pedidos", pedidos);
+        model.addAttribute("status", status);
+        return "home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError() {
+        return "redirect:/home";
     }
 }
