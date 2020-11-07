2efd369fd39e35cf8e6e658568e2bc555da270fc
wagner maidson <wagner.rmaidson@gmail.com>
2020-11-07 03:10:32 -0300

    refactor repository

--- src/main/java/br/com/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/mvc/mudi/repository/PedidoRepository.java
@@ -1,12 +1,16 @@
-package br.com.mvc.mudi.controller.repository;
+package br.com.mvc.mudi.repository;
 
+import br.com.mvc.mudi.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.mvc.mudi.model.Pedido;
 
+import java.util.List;
+
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    List<Pedido> findByStatus(StatusPedido status);
 }
