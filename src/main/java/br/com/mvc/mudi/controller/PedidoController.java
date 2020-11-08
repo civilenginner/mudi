c7e0b03c14dd7a43627348ee7db50db245ac609d
wagner maidson <wagner.rmaidson@gmail.com>
2020-11-08 20:55:31 -0300

    injected user repository

--- src/main/java/br/com/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/mvc/mudi/controller/PedidoController.java
@@ -2,10 +2,13 @@ package br.com.mvc.mudi.controller;
 
 import javax.validation.Valid;
 
+import br.com.mvc.mudi.model.User;
 import br.com.mvc.mudi.repository.PedidoRepository;
 import br.com.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.mvc.mudi.model.Pedido;
+import br.com.mvc.mudi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,17 +23,25 @@ public class PedidoController {
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("formulario")
     public String formulario(RequisicaoNovoPedido requisicao) {
         return "pedido/formulario";
 
     }
+
     @PostMapping("novo")
     public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
         if (result.hasErrors()) {
             return "pedido/formulario";
         }
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsername(username);
         Pedido pedido = requisicao.toPediddo();
+        pedido.setUser(user);
         pedidoRepository.save(pedido);
         return "redirect:/home";
     }
